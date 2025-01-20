ccf396404814a613b6848bc0a7a1938c08e4e232
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-21 03:18:24 +0800

    Site Update. [skip ci]

---
+++ contents/tp-comments.page-vue-render.js
@@ -0,0 +1,13 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('div',{attrs:{"id":"flex-body"}},[_c('div',{attrs:{"id":"content-wrapper"}},[_m(0),_v(" "),_c('p',[_v("Sorted based on the number of review comments given to "),_c('em',[_v("others'")]),_v(" PRs, but also showing comments on own PRs and other comments given. "),_c('strong',[_v("Updated weekly")]),_v(". Only those who gave at least one "),_c('tooltip',{scopedSlots:_u([{key:"content",fn:function(){return [_v("comments added to a specific place in the PR code")]},proxy:true}])},[_v("review comment")]),_v(" are listed in this dashboard.")],1),_v(" "),_c('div',{staticStyle:{"color":"red"}},[_v("No such file: /Users/zhuzikun/Desktop/dashboards/dashboards/contents/cs2103/tp-comments-panels-fragment.md\nMissing reference in /Users/zhuzikun/Desktop/dashboards/dashboards/contents/tp-comments.md")])])]),_v(" "),_m(1)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{staticClass:"display-4",attrs:{"id":"tp-comments-dashboard"}},[_v("tP comments dashboard"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#tp-comments-dashboard","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 21 Jan 2025, 3:17:45 GMT+8]")])])])}
+}];
+  
